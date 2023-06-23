e09044f64d313db8803833137dad52ecffc7b7e8
Kilderson Sena <dersonsena@gmail.com>
2023-06-23 19:06:20 -0300

    feat: updates

--- Domain/DomainEventDispatcher.php
+++ Domain/DomainEventDispatcher.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Astrotech\ApiBase\Domain;
 
-use Illuminate\Support\Facades\App;
 use Astrotech\ApiBase\Domain\Contracts\Event;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Astrotech\ApiBase\Exception\RuntimeException;
 use Psr\EventDispatcher\ListenerProviderInterface;
+use stdClass;
 
 final class DomainEventDispatcher implements EventDispatcherInterface
 {
@@ -24,7 +24,8 @@ final class DomainEventDispatcher implements EventDispatcherInterface
     public function dispatch(object $event): void
     {
         foreach ($this->listenerProvider->getListenersForEvent($event) as $className) {
-            $listenerObject = App::make($className);
+            // $listenerObject = App::make($className);
+            $listenerObject = new stdClass();
 
             if (!method_exists($listenerObject, 'handle')) {
                 throw new RuntimeException("Listeners class doesn't have 'handle' method");
