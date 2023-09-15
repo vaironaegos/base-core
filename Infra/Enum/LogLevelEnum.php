dc9d81bf34c237e7f8fb16be1db672daec35846f
Kilderson Sena <dersonsena@gmail.com>
2023-09-14 23:39:55 -0300

    feat: implementation of mongoDb Log implementation and refactorings

---
+++ Infra/Enum/LogLevelEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Astrotech\ApiBase\Infra\Enum;
+
+enum LogLevelEnum: int
+{
+    case FATAL = 0;
+    case ERROR = 1;
+    case WARNING = 2;
+    case INFO = 3;
+    case DEBUG = 4;
+    case TRACE = 5;
+}
