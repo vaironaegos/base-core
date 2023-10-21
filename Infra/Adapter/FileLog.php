948e3447f05dd1e72ebcd588edbd1922864d4f03
Kilderson Sena <dersonsena@gmail.com>
2023-10-21 11:59:19 -0300

    chore: adding default file logs path

--- Infra/Adapter/FileLog.php
+++ Infra/Adapter/FileLog.php
@@ -10,42 +10,42 @@ final class FileLog implements LogSystem
 {
     public function debug(string $message, array $options = []): void
     {
-        $fileName = $options['filename'] ?? 'fileLog.log';
+        $fileName = $options['filename'] ?? LOGS_PATH .  '/fileLog.log';
         $output = '[' . date('Y-m-d H:i:s') . '] DEBUG: ' . PHP_EOL . $message . PHP_EOL . PHP_EOL;
         file_put_contents($fileName, $output, FILE_APPEND);
     }
 
     public function trace(string $message, array $options = []): void
     {
-        $fileName = $options['filename'] ?? 'fileLog.log';
+        $fileName = $options['filename'] ?? LOGS_PATH .  '/fileLog.log';
         $output = '[' . date('Y-m-d H:i:s') . '] TRACE: ' . PHP_EOL . $message . PHP_EOL . PHP_EOL;
         file_put_contents($fileName, $output, FILE_APPEND);
     }
 
     public function info(string $message, array $options = []): void
     {
-        $fileName = $options['filename'] ?? 'fileLog.log';
+        $fileName = $options['filename'] ?? LOGS_PATH .  '/fileLog.log';
         $output = '[' . date('Y-m-d H:i:s') . '] INFO: ' . PHP_EOL . $message . PHP_EOL . PHP_EOL;
         file_put_contents($fileName, $output, FILE_APPEND);
     }
 
     public function warning(string $message, array $options = []): void
     {
-        $fileName = $options['filename'] ?? 'fileLog.log';
+        $fileName = $options['filename'] ?? LOGS_PATH .  '/fileLog.log';
         $output = '[' . date('Y-m-d H:i:s') . '] WARN: ' . PHP_EOL . $message . PHP_EOL . PHP_EOL;
         file_put_contents($fileName, $output, FILE_APPEND);
     }
 
     public function error(string $message, array $options = []): void
     {
-        $fileName = $options['filename'] ?? 'fileLog.log';
+        $fileName = $options['filename'] ?? LOGS_PATH .  '/fileLog.log';
         $output = '[' . date('Y-m-d H:i:s') . '] ERROR: ' . PHP_EOL . $message . PHP_EOL . PHP_EOL;
         file_put_contents($fileName, $output, FILE_APPEND);
     }
 
     public function fatal(string $message, array $options = []): void
     {
-        $fileName = $options['filename'] ?? 'fileLog.log';
+        $fileName = $options['filename'] ?? LOGS_PATH .  '/fileLog.log';
         $output = '[' . date('Y-m-d H:i:s') . '] FATAL: ' . PHP_EOL . $message . PHP_EOL . PHP_EOL;
         file_put_contents($fileName, $output, FILE_APPEND);
     }
