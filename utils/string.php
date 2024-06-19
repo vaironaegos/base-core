c2984ce5f903a1184d586c6327393249bececc02
Italo André <italoaaoliveira@gmail.com>
2024-06-19 11:59:04 -0300

    feat: creates function to generate random string

--- utils/string.php
+++ utils/string.php
@@ -209,3 +209,10 @@ if (!function_exists('env')) {
         return $value !== false ? $value : $default;
     }
 }
+
+if (!function_exists('randomString')) {
+    function randomString(int $length = 16): string
+    {
+        return substr(bin2hex(random_bytes($length)), 0, $length);
+    }
+}
